944fb40c4a6ffc9d13d43ea214e6ddcace55aeeb
d.danevskyi <d.danevskyi@gmail.com>
2018-02-01 12:23:22 +0200

    Add logging and commit hash saving

--- mag/experiment.py
+++ mag/experiment.py
@@ -1,5 +1,6 @@
 import os
-import shutil
+import sys
+import subprocess
 
 from mag.config import Config
 
@@ -17,15 +18,61 @@ class Experiment:
         
         self.experiments_dir = experiments_dir
 
-        os.makedirs(self.experiment_dir, exist_ok=True)
+        self._makedir() 
+        self._save_config()
+        self._save_git_commit_hash()
+
+    def _makedir(self):
+        os.makedirs(self.experiment_dir, exist_ok=False)
+
+    def _save_config(self):
         self.config.to_json(self.config_file)
 
+    def _save_git_commit_hash(self):
+        label = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
+
+        with open(self.git_hash_file, "w") as f:
+            f.write(label.decode())
+    
     @property
     def experiment_dir(self):
-
         return os.path.join(self.experiments_dir, self.config.identifier)
 
     @property
     def config_file(self):
-
         return os.path.join(self.experiment_dir, "config.json")
+
+    @property
+    def log_file(self):
+        return os.path.join(self.experiment_dir, "log")
+
+    @property
+    def git_hash_file(self):
+        return os.path.join(self.experiment_dir, "commit_hash")
+
+    def __enter__(self):
+        self.tee = Tee(self.log_file, "w")
+        return self
+
+    def __exit__(self, *args):
+        self.tee.close()
+
+
+class Tee:
+    """A helper class to duplicate stdout to a log file"""
+
+    def __init__(self, name, mode):
+        self.file = open(name, mode)
+        self.stdout = sys.stdout
+        sys.stdout = self
+
+    def close(self, *args):
+        sys.stdout = self.stdout
+        self.file.close()
+
+    def write(self, data):
+        self.file.write(data)
+        self.stdout.write(data)
+    
+    def flush(self):
+        self.file.flush()
--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -3,7 +3,6 @@ import json
 
 import pytest
 
-
 from mag.experiment import Experiment
 from mag.config import Config
 
@@ -36,10 +35,46 @@ def test_experiment_initialization(nested_dict_config, tmpdir):
 
     experiments_dir = tmpdir.join("experiments").strpath
 
-    experiment = Experiment(nested_dict_config, experiments_dir=experiments_dir)
+    experiment = Experiment(
+        nested_dict_config, experiments_dir=experiments_dir
+    )
 
     config = Config.from_json(os.path.join(
         experiments_dir, experiment.config.identifier, "config.json")
     )
 
     assert config.to_dict() == nested_dict_config
+
+
+def test_experiment_logging(nested_dict_config, tmpdir):
+
+    experiments_dir = tmpdir.join("experiments").strpath
+
+    with Experiment(
+            nested_dict_config,
+            experiments_dir=experiments_dir
+        ) as experiment:
+
+        print("test")
+
+    with open(experiment.log_file, "r") as f:
+        assert f.read().strip() == "test"
+
+    print("test2")
+    # check that nothing is logged when print is called
+    # outside with block
+    with open(experiment.log_file, "r") as f:
+        assert f.read().strip() == "test"
+
+
+def test_experiment_save_commit_hash(nested_dict_config, tmpdir):
+
+    experiments_dir = tmpdir.join("experiments").strpath
+
+    experiment = Experiment(
+        nested_dict_config, experiments_dir=experiments_dir
+    )
+
+    assert os.path.isfile(os.path.join(
+        experiment.experiment_dir, "commit_hash")
+    )
