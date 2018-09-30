129a781a4c640b7af6df37f9918464cf15a21034
Dmitriy <d.danevskyi@gmail.com>
2018-09-30 21:44:54 +0300

    Update tensorflow example

--- examples/mnist_tensorflow.py
+++ examples/mnist_tensorflow.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
@@ -21,7 +22,8 @@ class MnistClassifier:
             logits = self._forward(images)
             loss = self._create_loss(logits, targets)
             self.train_op = self._create_optimizer(loss)
-            self._create_summaries(loss)
+            accuracy = self._create_accuracy(logits, targets)
+            self._create_summaries(loss, accuracy)
             self._create_summary_writers()
             self._create_initializers()
             self._create_saver()
@@ -68,11 +70,12 @@ class MnistClassifier:
         with tf.variable_scope("forward"):
 
             net = images
-            net = tf.layers.dense(
-                net, self.config.network.hidden_units,
-                # dynamically get the activation from config
-                activation=getattr(tf.nn, self.config.network.activation)
-            )
+            for layer in range(self.config.network.n_layers):
+                net = tf.layers.dense(
+                    net, self.config.network.hidden_units,
+                    # dynamically get the activation from config
+                    activation=getattr(tf.nn, self.config.network.activation)
+                )
             net = tf.layers.dense(
                 net, self.config._n_classes, activation=None
             )
@@ -91,6 +94,14 @@ class MnistClassifier:
 
             return cross_entropy
 
+    def _create_accuracy(self, logits, targets):
+        with tf.name_scope("accuracy"):
+            correct = tf.equal(
+                tf.argmax(logits, axis=1),
+                tf.argmax(targets, axis=1))
+            accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+            return accuracy
+
     def _create_optimizer(self, loss):
         with tf.name_scope("optimizer"):
 
@@ -105,19 +116,28 @@ class MnistClassifier:
 
             return train_op
 
-    def _create_summaries(self, loss):
+    def _create_summaries(self, loss, accuracy):
         with tf.variable_scope("summaries"):
             # streaming mean helps to compute the mean across
             # all samples in the dataset
-            streaming_loss, self.update_loss = tf.metrics.mean(
-                loss
-            )
-            cross_entropy = tf.summary.scalar(
-                "loss",
-                streaming_loss
-            )
+            streaming_loss, update_loss = tf.metrics.mean(loss)
+            streaming_accuracy, update_accuracy = tf.metrics.mean(accuracy)
 
-            self.summary = tf.summary.merge([cross_entropy])
+            self.update_metrics = tf.group(update_loss, update_accuracy)
+
+            self.loss = streaming_loss
+            self.accuracy = streaming_accuracy
+
+            cross_entropy_summary = tf.summary.scalar(
+                "loss",
+                streaming_loss)
+
+            accuracy_summary = tf.summary.scalar(
+                "accuracy",
+                streaming_accuracy)
+
+            self.summary = tf.summary.merge([
+                cross_entropy_summary, accuracy_summary])
 
     def _create_initializers(self):
         self.variables_initializer = tf.global_variables_initializer()
@@ -160,7 +180,7 @@ class MnistClassifier:
         sess.run(self.local_variables_initializer)
         sess.run(self.train_initializer_op)
 
-        fetches = [self.train_op, self.update_loss]
+        fetches = [self.train_op, self.update_metrics]
 
         while True:
             try:
@@ -179,7 +199,7 @@ class MnistClassifier:
         sess.run(self.local_variables_initializer)
         sess.run(self.valid_initializer_op)
 
-        fetches = [self.update_loss]
+        fetches = [self.update_metrics]
 
         while True:
             try:
@@ -187,9 +207,12 @@ class MnistClassifier:
             except tf.errors.OutOfRangeError:
                 break
 
-        self.valid_writer.add_summary(
-            sess.run(self.summary), sess.run(self.global_step)
-        )
+        summary, loss, accuracy, global_step = sess.run([
+            self.summary, self.loss, self.accuracy, self.global_step])
+
+        self.valid_writer.add_summary(summary, global_step)
+
+        return loss, accuracy
 
     def fit(self):
 
@@ -208,39 +231,76 @@ class MnistClassifier:
         return tf.Session(graph=self.graph, config=config)
 
 
-with Experiment({
-    "_n_classes": 10,
-    "network": {
-        "hidden_units": 128,
-        "activation": "relu"
-    },
-    "train": {
-        "batch_size": 64,
-        "n_epochs": 10,
-        # to exclude the parameter from the identifier,
-        # start its name from the underscore
-        "_buffer_size": 128,
-        "learning_rate": 1e-3,
-        "momentum": 0.9
-    },
-    "validation": {
-        "_batch_size": 128
-    }
-}) as experiment:
+if __name__ == "__main__":
 
-    classifier = MnistClassifier(mnist, experiment)
-    classifier.fit()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    print("Finished!")    # will be logged to a file
+    parser.add_argument(
+        "--n_layers", type=int, default=1,
+        help="Number of layers of a neural network.")
+    parser.add_argument(
+        "--hidden_units", type=int, default=128,
+        help="Number of hidden units in each layer of a neural network.")
+    parser.add_argument(
+        "--activation", type=str, default="relu",
+        help="Activation function used in each layer of a neural network.")
+    parser.add_argument(
+        "--batch_size", type=int, default=32,
+        help="Minibatch size.")
+    parser.add_argument(
+        "--n_epochs", type=int, default=20,
+        help="Number of training epochs.")
+    parser.add_argument(
+        "--lr", type=float, default=1e-3,
+        help="Learning rate.")
+    parser.add_argument(
+        "--momentum", type=float, default=0.9,
+        help="Momentum value used in optimizer.")
 
+    args = parser.parse_args()
 
-# now, restore the experiment
-# relu|128|64|0.001|0.9|10 is a result of calling
-# experiment.config.identifier which shortly summarizes
-# the parameters listed in config
-with Experiment(resume_from="relu|128|64|0.001|0.9|10") as experiment:
-    classifier = MnistClassifier(mnist, experiment)
-    classifier.sess = classifier._setup_session()
-    classifier.load(classifier.sess)
+    with Experiment({
+        "_n_classes": 10,
+        "network": {
+            "n_layers": args.n_layers,
+            "hidden_units": args.hidden_units,
+            "activation": args.activation
+        },
+        "train": {
+            "batch_size": args.batch_size,
+            "n_epochs": args.n_epochs,
+            # to exclude the parameter from the identifier,
+            # start its name from the underscore
+            "_buffer_size": 128,
+            "learning_rate": args.lr,
+            "momentum": args.momentum
+        },
+        "validation": {
+            "_batch_size": 128
+        }
+    }) as experiment:
 
-    # now the model was successfully restored and ready for usage!
+        classifier = MnistClassifier(mnist, experiment)
+        classifier.fit()
+
+        print("Finished!")    # will be logged to a file
+
+        identifier = experiment.config.identifier
+
+    # Now, restore the experiment.
+    # Normally you might want to specify `resume_from` argument manually,
+    # typically by passing as a command line argument
+    with Experiment(resume_from=identifier) as experiment:
+        classifier = MnistClassifier(mnist, experiment)
+        classifier.sess = classifier._setup_session()
+        classifier.load(classifier.sess)
+
+        # now the model is successfully restored and ready for usage!
+
+        loss, accuracy = classifier._validation(classifier.sess)
+
+        # float call is required since both loss and accuracy have
+        # np.float32 type which is not json-serializable
+        experiment.register_result("loss", float(loss))
+        experiment.register_result("accuracy", float(accuracy))
