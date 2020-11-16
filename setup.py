1d63096901dde7f49eb3c946a6a538a208d8029f
Dmytro Danevskyi <d.danevskyi@gmail.com>
2020-11-16 09:18:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 import os
+import io
 from setuptools import setup, find_packages
 
 
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
 def load_requirements(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename), "r") as f:
+    with open(os.path.join(ROOT, filename), "r") as f:
         return f.read().splitlines()
+
+
+def load_readme():
+    readme_path = os.path.join(ROOT, "README.md")
+    with io.open(readme_path, encoding="utf-8") as f:
+        return f"\n{f.read()}"
 
 
 setup(
@@ -13,11 +23,14 @@ setup(
     packages=find_packages(),
     description=("A lightweight python library for keeping "
                  "track of numerical experiments"),
-     entry_points={
+    long_description=load_readme(),
+    long_description_content_type="text/markdown",
+    entry_points={
         "console_scripts": [
             "maggot=maggot.__main__:main",
         ],
     },
+    include_package_data=True,
     install_requires=load_requirements("requirements.txt"),
     author="Dmytro Danevskyi",
     author_email="d.danevskyi@gmail.com"
