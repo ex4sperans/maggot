8df36da57b350e9fb93c69a06ff0d08edcd7db12
Dmytro Danevskyi <d.danevskyi@gmail.com>
2020-11-16 08:59:35 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import os
 from setuptools import setup, find_packages
+
+
+def load_requirements(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename), "r") as f:
+        return f.read().splitlines()
+
 
 setup(
     name="maggot",
@@ -11,6 +18,7 @@ setup(
             "maggot=maggot.__main__:main",
         ],
     },
+    install_requires=load_requirements("requirements.txt"),
     author="Dmytro Danevskyi",
     author_email="d.danevskyi@gmail.com"
 )
